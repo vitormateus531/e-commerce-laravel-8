396464c80eaeaeca8439bd4c13e52d126f175102
Vitor Mateus <vitormateus15@hotmail.com>
2021-09-15 23:24:25 -0300

    ajuste label

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -93,7 +93,7 @@
                         <x-slot name="content">
                             <!-- Account Management -->
                             <div class="block px-4 py-2 text-xs text-gray-400">
-                                {{ __('Manage Account') }}
+                                {{ __('gerenciar conta') }}
                             </div>
 
                             <x-jet-dropdown-link href="{{ route('profile.show') }}">
