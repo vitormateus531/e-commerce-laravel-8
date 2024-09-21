0b23c5787920e1f0ced38cb6170cc2cf67c19b23
Vitor Mateus <vitormateus15@hotmail.com>
2024-09-21 14:23:03 -0300

    ajustes numeros painel de controle

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,15 +11,15 @@
                 <div class="flex justify-center space-x-10">
                     <div class="justify-center">
                         <i class="fas fa-users text-5xl text-blue-600"></i>
-                        <label class="mt-14 absolute -ml-10 font-bold">{{$funcionarios}}</label>
+                        <label class="mt-14 absolute -ml-8 font-bold">{{$funcionarios}}</label>
                     </div>
                     <div class="justify-center">
                         <i class="fas fa-dumpster text-5xl text-green-600"></i>
-                        <label class="mt-14 absolute -ml-9 font-bold">{{$lojas}}</label>
+                        <label class="mt-14 absolute -ml-8 font-bold">{{$lojas}}</label>
                     </div>
                     <div class="justify-center">
                         <i class="fas fa-shopping-basket text-5xl text-gray-600"></i>
-                        <label class="mt-14 absolute -ml-9 font-bold">{{$produtos}}</label>
+                        <label class="mt-14 absolute -ml-8 font-bold">{{$produtos}}</label>
                     </div>
                 </div>
             </div>
